d2209de44e933406f5dba6565b8c20c74def0c2a
Emil Poulsen <emil-poulsen@hotmail.com>
2022-11-06 00:27:06 -0400

    bump version

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace ConfigAR.Backend.Controllers
         [HttpGet(Name = "/")]
         public string    Get()
         {
-            return "Welcome to ConfigAR Backend v.0.1!";
+            return "Welcome to ConfigAR Backend v.0.3!";
         }
     }
 }
